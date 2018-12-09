64c1a221b45923a0d531e48a4db28be1796ca854
Lei Pan <panlei7@outlook.com>
2018-12-09 22:49:33 +0800

    basic function

---
+++ build/src/CMakeFiles/c4we.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lei/Work/c4we/src/inversion.cc" "/home/lei/Work/c4we/build/src/CMakeFiles/c4we.dir/inversion.cc.o"
+  "/home/lei/Work/c4we/src/problem.cc" "/home/lei/Work/c4we/build/src/CMakeFiles/c4we.dir/problem.cc.o"
+  "/home/lei/Work/c4we/src/statistics.cc" "/home/lei/Work/c4we/build/src/CMakeFiles/c4we.dir/statistics.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib64/cmake/yaml-cpp/../../../include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
